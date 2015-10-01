87a12700991119708470e48d139fe61870f43b3a
akil <akil@akila>
2015-10-01 15:02:24 +0700

    Version 1.0

---
+++ icontrol.php
@@ -0,0 +1,28 @@
+<?php
+
+
+//namespace fai\modules\backoffice;
+
+
+/**
+ * @author akil
+ * @version 1.0
+ * @created 05-Aug-2015 3:38:43 PM
+ */
+interface IControl
+{
+
+	public function add();
+
+	public function edit();
+
+	public function find();
+
+	public function index();
+
+	public function remove();
+
+	public function save();
+
+}
+?>
